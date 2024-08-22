0f671c94fc5d5250a4ccd30360ac32eb02f2c7e0
Rafael Gomes <raafaelgomees10@gmail.com>
2024-08-22 17:23:32 -0300

    cleaning project, installing styled-components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import "@/styles/globals.css";
+import { GlobalStyles } from "@/styles/globalStyles";
+import { theme } from "@/styles/theme";
+import { ThemeProvider } from "styled-components";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle` 
+* {
+	box-sizing: border-box;
+	font-family: "Poppins", sans-serif;
+	font-size: 62.5%;
+	list-style: none;
+	margin: 0;
+	padding: 0;
+	color: #000;
+}
+
+html,
+body {
+
+}
+`;
